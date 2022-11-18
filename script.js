672399617cc15e5037c55d84d747bcd5b48c3dbc
samuelp244 <samuelp244@gmail.com>
2022-11-19 01:45:47 +0530

    adding build file

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
-
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-sun')
-})
